b1a91311e25cbd26b26e2202f5921d8f850c5811
datawithali <109055148+datawithali@users.noreply.github.com>
2023-01-09 16:34:43 -0600

    Update logic.js

--- Simple_Map/static/logic.js
+++ Simple_Map/static/logic.js
@@ -8,7 +8,38 @@ let map = L.map("mapid", {
     ],
     zoom: 4
   });
-
+// An array containing each city's location, state, and population.
+let cities = [{
+  location: [40.7128, -74.0059],
+  city: "New York City",
+  state: "NY",
+  population: 8398748
+},
+{
+  location: [41.8781, -87.6298],
+  city: "Chicago",
+  state: "IL",
+  population: 2705994
+},
+{
+  location: [29.7604, -95.3698],
+  city: "Houston",
+  state: "TX",
+  population: 2325502
+},
+{
+  location: [34.0522, -118.2437],
+  city: "Los Angeles",
+  state: "CA",
+  population: 3990456
+},
+{
+  location: [33.4484, -112.0740],
+  city: "Phoenix",
+  state: "AZ",
+  population: 1660272
+}
+];
   // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('pk.eyJ1IjoiYWlicmFoaW1vdiIsImEiOiJjbDEyYXVkdGkxaW9kM3FvMGt2Mng3MWNhIn0.-Cv8ksrN-HPu3KR3QhXKxw', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
